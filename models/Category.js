1a475b6c836551b6ef12c2bd6e7ca0051c09c54e
rhx1138 <ryanjhendrix@gmail.com>
2022-11-04 13:01:23 -0400

    defined columns in Category.js, getting error

--- models/Category.js
+++ models/Category.js
@@ -7,8 +7,15 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+      
+    
   },
-  {
+  { 
     sequelize,
     timestamps: false,
     freezeTableName: true,
